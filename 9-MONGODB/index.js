d436d50ff10b1a303a54f9fbaebcdcab719ff41a
Shreyash Jaiswal <shreyash5709@gmail.com>
2025-10-04 17:56:03 +0530

    data schema design and ho toi store datain mongodb

--- 9-MONGODB/index.js
+++ 9-MONGODB/index.js
@@ -1,3 +1,39 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/test");
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+// let user1 = new User({
+//     name: "Adam",
+//     email: "adam@gmail.com",
+//     age: 19,
+// });
+// user1.save();
+
+// User.insertMany([
+//     {name: "Amit", email: "amit@gmail.com", age: 20},
+//     {name: "Suman", email: "suman@gmail.com", age: 18},
+//     {name: "Rahul", email: "rahul@gmail.com", age: 19},
+// ])
+// .then((res) => {
+//     console.log(res);
+// });
+
+User.find({age: {$gte: 19}}).then((res) => {
+    console.log("Resulte are here : ",res);
+});
