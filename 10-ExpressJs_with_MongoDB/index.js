abaa2221e9db35677445a79e4e2726e78392d02f
Shreyash Jaiswal <shreyash5709@gmail.com>
2025-10-04 23:07:31 +0530

    msg editing and new msg adding done

---
+++ 10-ExpressJs_with_MongoDB/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+main()
+    .then(() => console.log("Connection successful"))
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Whatsapp');
+}
+
+// All chats
+app.get("/chats", async(req, res) => {
+    let chats = await Chat.find();
+    res.render("chat.ejs", {chats});
+});
+
+// writing chat details
+app.get("/chats/new", async(req, res) => {
+    res.render("new.ejs");
+});
+
+//add new chat
+app.post("/chats", async(req, res) => {
+    let {from, to, msg} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        createdAt: new Date()
+    });
+    newChat.save().catch(err => console.log(err));
+    res.redirect("/chats");
+});
+
+// editin chat
+app.get("/chats/:id/edit", async(req, res) => {
+    let id = req.params.id;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+});
+// update chat
+app.post("/chats/:id", async(req, res) => {
+    let id = req.params.id;
+    let {msg : newMsg} = req.body;
+    await Chat.findByIdAndUpdate(id, 
+        {msg: newMsg}, 
+        {runValidators: true, new: true}
+    );
+    res.redirect("/chats");
+});
+
+app.get("/", (req, res) => {
+    res.send("working");
+});
+
+app.listen(8080, () => {
+    console.log("listening on port 8080 : ");
+});
