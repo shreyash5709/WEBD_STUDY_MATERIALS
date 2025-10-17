3c471c5af6d37efcb93b6a88f474d78318515d26
Shreyash Jaiswal <shreyash5709@gmail.com>
2025-10-17 22:09:02 +0530

    conditional statement

--- 12-ReactJS/basic-react-app/src/Product.jsx
+++ 12-ReactJS/basic-react-app/src/Product.jsx
@@ -6,6 +6,7 @@ function Product({title, price, features}){
             <h3>{title}</h3>
             <h5>{price}</h5>
             <ul>{features.map((feature) => <li>{feature}</li>)}</ul>
+            {price>=30000 ? <p>Discount: 5%</p> : null}
         </div>
     )
 }
