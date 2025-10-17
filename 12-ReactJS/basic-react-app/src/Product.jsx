35669a268e97c7ba5f94cc0389af48c8afbe8432
Shreyash Jaiswal <shreyash5709@gmail.com>
2025-10-17 21:40:25 +0530

    array and object passing done

--- 12-ReactJS/basic-react-app/src/Product.jsx
+++ 12-ReactJS/basic-react-app/src/Product.jsx
@@ -1,9 +1,12 @@
 import "./Product.css";
-function Product({title, price}){
+function Product({title, price, feature, feature2}){
+    console.log()
     return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>{price}</h5>
+            <p>{feature}</p>
+            <p>{feature2.a}</p>
         </div>
     )
 }
--- 12-ReactJS/basic-react-app/src/ProductTab.jsx
+++ 12-ReactJS/basic-react-app/src/ProductTab.jsx
@@ -1,10 +1,11 @@
 import Product from "./Product"
 function ProductTab() {
+    let option = ["Hi-tech", "Very advanced"];
     return(
         <>
-            <Product title={"Phone"} price={"30k"}/>
-            <Product title={"Laptop"} price={750000}/>
-            <Product title={"Tab"} price={25000}/>
+            <Product title={"Phone"} price={"30k"} feature={option} feature2={{a:"hi-tech", b:"very advanced"}}/>
+            <Product title={"Laptop"} price={750000} feature={option} feature2={{a:"hi-tech", b:"very advanced"}}/>
+            <Product title={"Tab"} price={25000} feature={option} feature2={{a:"hi-tech", b:"very advanced"}}/>
         </>
     )
 }
