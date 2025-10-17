6e20adb86726ecbafe5e02b5debbb0812c9f2f73
Shreyash Jaiswal <shreyash5709@gmail.com>
2025-10-17 22:17:49 +0530

    Dynamic style applying on the basis of some conditions

--- 12-ReactJS/basic-react-app/src/Product.jsx
+++ 12-ReactJS/basic-react-app/src/Product.jsx
@@ -1,8 +1,9 @@
 import "./Product.css";
 function Product({title, price, features}){
     // let list = features.map((feature) => <li>{feature}</li>);
+    let styles = {backgroundColor: price>=30000 ? "lightblue" : "lightgreen" }
     return(
-        <div className="Product">
+        <div className="Product" style={styles}>
             <h3>{title}</h3>
             <h5>{price}</h5>
             <ul>{features.map((feature) => <li>{feature}</li>)}</ul>
