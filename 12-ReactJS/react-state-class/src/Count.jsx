77cbc771cfeb6bab749d7c218dff2f54a31e96e6
Shreyash Jaiswal <shreyash5709@gmail.com>
2025-10-21 19:35:06 +0530

    useState done

--- 12-ReactJS/react-state-class/src/Count.jsx
+++ 12-ReactJS/react-state-class/src/Count.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
+
 export default function Count(){
-    let count = 0;
+    let [count, setCount] = useState(0);
 
     function incCount(){
-        count += 1;
+        setCount(count+1);
         console.log(count);
     }
 
     return(
         <>
-        <h3>Count: {count}</h3>
+        <h3>Count: { count }</h3>
         <button onClick={incCount}>Increase Count</button>
         </>
     )
